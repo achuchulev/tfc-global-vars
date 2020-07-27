314a536e9dd82bababbfc233ae53a44f1abb641c
Atanas Chuchulev <atanas.v4@gmail.com>
2020-07-27 11:19:38 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,19 +4,19 @@ provider "tfe" {
   token = var.tfe_token # the token needs to have permissions to write variables to the workspaces
 }
 
-data "tfe_workspace_ids" "ids" {
+data "tfe_workspace_ids" "all" {
   names        = ["*"]
   organization = "atanasc-01" #"my-org-name"
 }
 
 module "ws1" {
   source = "./the_variables"
-  #name = "aws-shared-credentials-file" #"ws1"
-  the_workspace_id = data.tfe_workspace_ids.external_ids["aws-shared-credentials-file"]
+  #name = "aws-shared-credentials-file"
+  the_workspace_id = data.tfe_workspace_ids.all.external_ids["aws-shared-credentials-file"]
 }
 
 module "ws2" {
   source = "./the_variables"
   #name = "terraform-null-localexec"
-  the_workspace_id = data.tfe_workspace_ids.external_ids["terraform-null-localexec"] #["ws2"]
+  the_workspace_id = data.tfe_workspace_ids.all.external_ids["terraform-null-localexec"]
 }
