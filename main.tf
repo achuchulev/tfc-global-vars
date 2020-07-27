025deb0ecc0d298b05b96cabf9e79e7ac2c58d6d
Atanas Chuchulev <atanas.v4@gmail.com>
2020-07-27 11:09:02 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+variable "tfe_token" {}
+
+provider "tfe" {
+  token = var.tfe_token # the token needs to have permissions to write variables to the workspaces
+}
+
+data "tfe_workspace_ids" "ids" {
+  names        = ["*"]
+  organization = "my-org-name"
+}
+
+module "ws1" {
+  source = "./the_variables"
+  name = "ws1"
+  the_workspace_id = data.tfe_workspace_ids.ids["ws1"]
+}
+
+module "ws2" {
+  source = "./the_variables"
+  name = "ws2"
+  the_workspace_id = data.tfe_workspace_ids.ids["ws2"]
+}
