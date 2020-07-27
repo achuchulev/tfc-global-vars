c8b2dcaa215a87031ee7ad7707c02d7f30ef4277
Atanas Chuchulev <atanas.v4@gmail.com>
2020-07-27 11:36:53 +0300

    Update variables.tf

--- the_variables/variables.tf
+++ the_variables/variables.tf
@@ -4,6 +4,7 @@ resource "tfe_variable" "v1" {
   key          = "AWS_ACCESS_KEY_ID"
   value        = "access_key_value"
   category     = "env"
+  sensitive    = "true"
   workspace_id = var.the_workspace_id
 }
 
@@ -11,5 +12,6 @@ resource "tfe_variable" "v2" {
   key          = "AWS_SECRET_ACCESS_KEY"
   value        = "secret_key_value"
   category     = "env"
+  sensitive    = "true"
   workspace_id = var.the_workspace_id
 }
