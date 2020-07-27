c76e475f8d536133b38c1a98cc1930ec890b58b9
Atanas Chuchulev <atanas.v4@gmail.com>
2020-07-27 11:08:24 +0300

    Create variables.tf

---
+++ the_variables/variables.tf
@@ -0,0 +1,15 @@
+variable "the_workspace_id" {}
+
+resource "tfe_variable" "v1" {
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = "access_key_value"
+  category     = "env"
+  workspace_id = var.the_workspace_id
+}
+
+resource "tfe_variable" "v2" {
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = "secret_key_value"
+  category     = "env"
+  workspace_id = var.the_workspace_id
+}
